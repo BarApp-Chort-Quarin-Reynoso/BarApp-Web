48af39c11299b360db5d5f5f3946577e400b533e
Valen <valenreynoso17@gmail.com>
2024-03-02 16:52:06 -0300

    First commit

---
+++ src/main/java/com/barapp/web/BarappWebApplication.java
@@ -0,0 +1,20 @@
+package com.barapp.web;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Push
+@SpringBootApplication
+@Theme(value = "barapp-web")
+@NpmPackage(value = "@fontsource/nunito-sans", version = "4.5.0")
+public class BarappWebApplication implements AppShellConfigurator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarappWebApplication.class, args);
+	}
+
+}
