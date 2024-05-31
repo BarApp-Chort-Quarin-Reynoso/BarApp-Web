bdf987f81aae872708492d18636b968f736ffb0e
ChortJulio <chortjulio@gmail.com>
2024-05-31 18:53:52 -0300

    Add update user photo endpoint
    Falta pasar la lógica del Firebase Storage acá, actualmente se continúa haciendo en mobile.

--- src/main/java/com/barapp/web/controllers/UsuarioRestController.java
+++ src/main/java/com/barapp/web/controllers/UsuarioRestController.java
@@ -12,8 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -55,5 +57,15 @@ public class UsuarioRestController extends BaseController<UsuarioApp> {
       } catch (Exception e) {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
       }        
+    }
+
+    @PatchMapping("/{id}/foto")
+    public ResponseEntity<Void> updateFoto(@PathVariable String id, @RequestParam("foto") String foto) {
+      try {
+        this.usuarioService.updateFoto(id, foto);
+        return new ResponseEntity<>(HttpStatus.OK);
+      } catch (Exception e) {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
     }
 }
