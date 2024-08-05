49ba8fd062b94dae6c32ab438aefa38fe9acee3f
ChortJulio <chortjulio@gmail.com>
2024-08-04 23:02:43 -0300

    Remove attributes from User (mobile)
    Se removieron atributos innecesarios del modelo y la entidad del Usuario mobile. Se entenderá más a la hora de hacer el registrar a través de la API.

--- src/main/java/com/barapp/web/data/entities/UsuarioEntity.java
+++ src/main/java/com/barapp/web/data/entities/UsuarioEntity.java
@@ -1,7 +1,5 @@
 package com.barapp.web.data.entities;
 
-import java.util.List;
-
 import lombok.*;
 
 @Getter
@@ -12,11 +10,6 @@ import lombok.*;
 public class UsuarioEntity extends BaseEntity {
     private String nombre;
     private String apellido;
-    private String idUsuario;
     private String idDetalleUsuario;
     private String foto;
-    private List<String> busquedasRecientes;
-    private List<String> idRestaurantesFavoritos;
-    private String mail;
-    private String telefono;
 }
