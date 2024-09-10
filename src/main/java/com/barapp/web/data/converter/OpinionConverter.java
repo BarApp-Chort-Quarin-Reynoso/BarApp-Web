9d7a44e3703530f2f7978ea385cc9ff1fff9ca5c
ChortJulio <chortjulio@gmail.com>
2024-09-10 19:54:47 -0300

    Add attributes to Opinion model
    Se agregaron nuevos atributos al modelo y entidad Opinion, los cuales son horario, cantidadPersonas, tipoComida y características.

--- src/main/java/com/barapp/web/data/converter/OpinionConverter.java
+++ src/main/java/com/barapp/web/data/converter/OpinionConverter.java
@@ -1,16 +1,19 @@
 package com.barapp.web.data.converter;
 
 import com.barapp.web.data.entities.OpinionEntity;
+import com.barapp.web.model.Horario;
 import com.barapp.web.model.Opinion;
 import com.barapp.web.model.UsuarioApp;
+import com.barapp.web.model.enums.TipoComida;
 import com.barapp.web.utils.FormatUtils;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class OpinionConverter implements BaseConverter<Opinion, OpinionEntity> {
-
     public OpinionEntity toEntity(Opinion dto) {
-        return OpinionEntity.builder()
+        return OpinionEntity
+            .builder()
                 .comentario(dto.getComentario())
                 .nota(dto.getNota())
                 .idUsuario(dto.getUsuario().getId())
@@ -19,16 +22,29 @@ public class OpinionConverter implements BaseConverter<Opinion, OpinionEntity> {
                 .apellidoUsuario(dto.getUsuario().getApellido())
                 .foto(dto.getUsuario().getFoto())
                 .fecha(dto.getFecha().format(FormatUtils.persistenceDateFormatter()))
+                .caracteristicas(dto.getCaracteristicas())
+                .horario(dto.getHorario().getHorario().toString())
+                .tipoComida(dto.getHorario().getTipoComida().toString())
+                .cantidadPersonas(dto.getCantidadPersonas())
                 .build();
     }
 
     public Opinion toDto(OpinionEntity entity) {
-        return Opinion.builder()
+        return Opinion
+            .builder()
                 .idRestaurante(entity.getIdRestaurante())
                 .comentario(entity.getComentario())
                 .nota(entity.getNota())
                 .fecha(LocalDate.parse(entity.getFecha(), FormatUtils.persistenceDateFormatter()))
-                .usuario(UsuarioApp.builder()
+                .caracteristicas(entity.getCaracteristicas())
+                .cantidadPersonas(entity.getCantidadPersonas())
+                .horario(Horario
+                    .builder()
+                        .horario(LocalTime.parse(entity.getHorario()))
+                        .tipoComida(TipoComida.valueOf(entity.getTipoComida()))
+                        .build())
+                .usuario(UsuarioApp
+                    .builder()
                         .id(entity.getIdUsuario())
                         .nombre(entity.getNombreUsuario())
                         .apellido(entity.getApellidoUsuario())
